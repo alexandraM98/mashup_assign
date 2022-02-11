57989c40dfd65a8e01c6bc303a5d47cd7ccb5727
Alex <alexandra_mitu98@yahoo.com>
2022-02-11 12:49:34 +0000

    Commit

--- index.js
+++ index.js
@@ -8,31 +8,10 @@ app.use(express.static('public'));
 //Enabling dotenv to use environment variables
 require('dotenv').config();
 
-//Using the twitter-lite package to access the Twitter API
-const Twitter = require('twitter-lite');
-
 //Setting the port for the app at localhost:3000
 const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
   });
 
-
-const language = require('@google-cloud/language');
-const languageClient = new language.LanguageServiceClient();
-
-async function getSentiment(text) {
-    const document = {
-        content: text,
-        type: 'PLAIN_TEXT',
-    };
-
-    // Detects the sentiment of the text
-    const [result] = await languageClient.analyzeSentiment({document: document});
-    const sentiment = result.documentSentiment;
-
-    return sentiment.score;
-}
-
-getSentiment("I HATE MESSI");
 
--- public/script.js
+++ public/script.js
@@ -7,9 +7,9 @@ const url = 'https://wavescap.com/api/asset/WAVES.json';
       const response = await fetch(url);
       const data = await response.json();
       
-      document.getElementById('total_cap').textContent = (Math.round((data.data['lastPrice_usd-n']) * 100) / 100).toFixed(2) + ' USD';
-      document.getElementById('assets_cap').textContent = (Math.round((data.data.lastPrice_gbpn) * 100) / 100).toFixed(2) + ' GBP';
-      document.getElementById('gateways_cap').textContent = (Math.round((data.data.lastPrice_eurn) * 100) / 100).toFixed(2) + ' EUR';
+      document.getElementById('price_usd').textContent = (Math.round((data.data['lastPrice_usd-n']) * 100) / 100).toFixed(2) + ' USD';
+      document.getElementById('price_gbp').textContent = (Math.round((data.data.lastPrice_gbpn) * 100) / 100).toFixed(2) + ' GBP';
+      document.getElementById('price_eur').textContent = (Math.round((data.data.lastPrice_eurn) * 100) / 100).toFixed(2) + ' EUR';
       document.getElementById('waves_desc').textContent = data.desc;
 
     }
