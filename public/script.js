ff5ac8d44efe7eb5b9ef1649d95960e5028c67cc
Alex <alexandra_mitu98@yahoo.com>
2022-02-11 11:42:11 +0000

    Added moving stats

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
 console.log("Script loaded.");
+
+const url = 'https://wavescap.com/api/asset/WAVES.json';
+
+  async function getData() {
+
+      const response = await fetch(url);
+      const data = await response.json();
+      
+      document.getElementById('total_cap').textContent = (Math.round((data.data['lastPrice_usd-n']) * 100) / 100).toFixed(2) + ' USD';
+      document.getElementById('assets_cap').textContent = (Math.round((data.data.lastPrice_gbpn) * 100) / 100).toFixed(2) + ' GBP';
+      document.getElementById('gateways_cap').textContent = (Math.round((data.data.lastPrice_eurn) * 100) / 100).toFixed(2) + ' EUR';
+      document.getElementById('waves_desc').textContent = data.desc;
+
+    }
+  getData();
